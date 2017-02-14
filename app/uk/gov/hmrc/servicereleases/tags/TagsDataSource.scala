423bafe8953ad0ffc04ebeea41da29ceec2bd4cb
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2017-02-14 11:51:54 +0000

    [PS] Updated license headers for 2017

--- app/uk/gov/hmrc/servicereleases/tags/TagsDataSource.scala
+++ app/uk/gov/hmrc/servicereleases/tags/TagsDataSource.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 HM Revenue & Customs
+ * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -84,7 +84,3 @@ class GitConnector(gitClient: GitClient, githubApiClient: GithubApiClient, ident
       githubApiClient.getReleases(organisation, repoName)
     }
 }
-
-
-
-
--- test/uk/gov/hmrc/servicereleases/services/CatalogueConnectorSpec.scala
+++ test/uk/gov/hmrc/servicereleases/services/CatalogueConnectorSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 HM Revenue & Customs
+ * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -64,5 +64,3 @@ class CatalogueConnectorSpec extends WordSpec with Matchers with WireMockSpec wi
     }
   }
 }
-
-
