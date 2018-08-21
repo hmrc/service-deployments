61853e009f3f1346b4dc5d91b9cbff161af1863d
Jakob Grunig <jakob.grunig@digital.hmrc.gov.uk>
2018-08-21 14:32:14 +0100

    PLATOPS-1542 - Updated deprecated dependencies

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -20,22 +20,13 @@ private object AppDependencies {
   import play.sbt.PlayImport._
   import play.core.PlayVersion
 
-  private val domainVersion                = "5.1.0"
-  private val microserviceBootstrapVersion = "6.7.0"
-  private val playUrlBindersVersion        = "2.1.0"
-  private val playReactivemongoVersion     = "6.0.0"
-  private val githubClientVersion          = "1.21.0"
-  private val gitClientVersion             = "0.6.0"
-
-  private val hmrcTestVersion = "2.4.0"
-
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25"  % "1.5.0",
-    "uk.gov.hmrc" %% "git-client"         % gitClientVersion,
-    "uk.gov.hmrc" %% "github-client"      % githubClientVersion,
+    "uk.gov.hmrc" %% "bootstrap-play-25"  % "1.7.0",
+    "uk.gov.hmrc" %% "git-client"         % "0.6.0",
+    "uk.gov.hmrc" %% "github-client"      % "1.21.0",
     "uk.gov.hmrc" %% "mongo-lock"         % "5.1.0",
-    "uk.gov.hmrc" %% "domain"             % domainVersion,
+    "uk.gov.hmrc" %% "domain"             % "5.2.0",
     "uk.gov.hmrc" %% "play-reactivemongo" % "6.2.0"
   )
 
@@ -48,7 +39,7 @@ private object AppDependencies {
     def apply() =
       new TestDependencies {
         override lazy val test = Seq(
-          "uk.gov.hmrc"            %% "hmrctest"           % hmrcTestVersion     % scope,
+          "uk.gov.hmrc"            %% "hmrctest"           % "2.4.0"             % scope,
           "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.0"             % scope,
           "org.pegdown"            % "pegdown"             % "1.5.0"             % scope,
           "com.github.tomakehurst" % "wiremock"            % "1.52"              % scope,
@@ -59,20 +50,5 @@ private object AppDependencies {
       }.test
   }
 
-  object IntegrationTest {
-    def apply() =
-      new TestDependencies {
-
-        override lazy val scope: String = "it"
-
-        override lazy val test = Seq(
-          "uk.gov.hmrc"       %% "hmrctest"  % hmrcTestVersion     % scope,
-          "org.scalatest"     %% "scalatest" % "2.2.6"             % scope,
-          "org.pegdown"       % "pegdown"    % "1.5.0"             % scope,
-          "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
-        )
-      }.test
-  }
-
-  def apply() = compile ++ Test() ++ IntegrationTest()
+  def apply() = compile ++ Test()
 }
