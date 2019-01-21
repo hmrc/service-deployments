4e2d9064e27e33de4875cfb15efc1faa67687968
Ian McShane <ianmcshane@live.co.za>
2019-01-21 10:52:00 +0000

    fix: Update year in copyright notice.

--- app/uk/gov/hmrc/servicedeployments/WhatIsRunningWhereController.scala
+++ app/uk/gov/hmrc/servicedeployments/WhatIsRunningWhereController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 HM Revenue & Customs
+ * Copyright 2019 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,6 +25,7 @@ import uk.gov.hmrc.servicedeployments.deployments.ServiceDeploymentInformation.f
 import scala.concurrent.ExecutionContext.Implicits.global
 
 import scala.concurrent.ExecutionContext
+
 @Singleton
 class WhatIsRunningWhereController @Inject()(
   whatIsRunningWhereRepository: WhatIsRunningWhereRepository,
