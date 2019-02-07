99a2a8afe7c7cd3fff8274a134af7e2e38ea5414
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2019-02-07 11:29:56 +0000

    bump mongo version to 0.16.1

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,19 +10,19 @@ private object AppDependencies {
     ws,
     "uk.gov.hmrc" %% "bootstrap-play-26"  % bootstrapPlayVersion,
     "uk.gov.hmrc" %% "git-client"         % "0.8.0",
-    "uk.gov.hmrc" %% "mongo-lock"         % "6.1.0-play-26",
+    "uk.gov.hmrc" %% "mongo-lock"         % "6.7.0-play-26",
     "uk.gov.hmrc" %% "domain"             % "5.2.0"
   )
 
   val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-play-26"  % bootstrapPlayVersion % Test classifier "tests",
-    "uk.gov.hmrc"            %% "hmrctest"           % "3.2.0"              % "test",
+    "uk.gov.hmrc"            %% "hmrctest"           % "3.2.0"              % Test,
     "org.scalatest"          %% "scalatest"          % "3.0.4"              % Test,
     "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2"              % Test,
-    "org.pegdown"            % "pegdown"             % "1.5.0"              % "test",
-    "com.github.tomakehurst" % "wiremock"            % "1.52"               % "test",
-    "com.typesafe.play"      %% "play-test"          % PlayVersion.current  % "test",
-    "uk.gov.hmrc"            %% "reactivemongo-test" % "4.1.0-play-26"      % "test",
-    "org.mockito"            % "mockito-all"         % "1.10.19"            % "test"
+    "org.pegdown"            %  "pegdown"            % "1.5.0"              % Test,
+    "com.github.tomakehurst" %  "wiremock"           % "1.52"               % Test,
+    "com.typesafe.play"      %% "play-test"          % PlayVersion.current  % Test,
+    "uk.gov.hmrc"            %% "reactivemongo-test" % "4.6.0-play-26"      % Test,
+    "org.mockito"            %  "mockito-all"        % "1.10.19"            % Test
   )
 }
